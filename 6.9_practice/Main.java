60d6ac96f9915d7f0b473cf0d48774be0dd93d52
Miyazaki <mr.teruterubouz@gmail.com>
2022-03-23 23:22:16 +0900

    [6.9] 練習問題の追加

---
+++ 6.9_practice/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+  public static void main(String[] args) throws Exception {
+    doWarusa();
+    doTogame();
+    callDeae();
+    showMondokoro();
+  }
+
+  public static void doWarusa() {
+    System.out.println("きなこでござる。食えませんがの。");
+  }
+
+  public static void doTogame() {
+    System.out.println("この老いぼれの目はごまかせませんぞ。");
+  }
+
+  public static void callDeae() {
+    System.out.println("ええい、こしゃくな。曲者だ！であえい！");
+  }
+
+  public static void showMondokoro() throws Exception {
+    System.out.println("飛車さん、角さん。もういいでしょう。");
+    System.out.println("この紋所が目に入らぬか！");
+    doTogame(); // もう一度、とがめる
+  }
+}
