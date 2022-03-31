cfc91a4dd672b5dc2cecd5ae98f246be08a60ec4
Miyazaki <mr.teruterubouz@gmail.com>
2022-04-01 00:04:08 +0900

    [10.7] 練習10-1 カプセル化

--- 10.7_practice/Wizard.java
+++ 10.7_practice/Wizard.java
@@ -1,12 +1,44 @@
 public class Wizard {
-    int hp;
-    int mp;
-    String name;
-    Wand wand;
+    private int hp;
+    private int mp;
+    private String name;
+    private Wand wand;
+
+    int getHp() {
+        return this.hp;
+    }
+
+    void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    int getMp() {
+        return this.mp;
+    }
+
+    void setMp(int mp) {
+        this.mp = mp;
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    Wand getWand() {
+        return this.wand;
+    }
+
+    void setWand(Wand wand) {
+        this.wand = wand;
+    }
 
     void heal(Hero h) {
         int basePoint = 10;
-        int recovPoint = (int) (basePoint * this.wand.power);
+        int recovPoint = (int) (basePoint * this.wand.getPower());
 
         h.setHp(h.getHp() + recovPoint);
         System.out.println(h.getName() + "のHPを" + recovPoint + "回復した！");
