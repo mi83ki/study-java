e80fda870783508e181aed24a5cf08c44409f17f
Miyazaki <mr.teruterubouz@gmail.com>
2022-04-06 22:20:50 +0900

    [12.6] 問題12-2

---
+++ 12.6_practice/Asset.java
@@ -0,0 +1,20 @@
+public class Asset {
+    private String name;
+    private int price;
+
+    // コンストラクタ
+    public Asset(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // getterメソッド
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+}
